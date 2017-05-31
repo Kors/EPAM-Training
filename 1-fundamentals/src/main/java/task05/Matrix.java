302041e56f5689d09667a471746d9314ac0157a1
Kors <i.korsikov@spb.ant-inform.ru>
2017-05-31 17:28:55 +0300

    1 - matrix (task 5)

---
+++ 1-fundamentals/src/main/java/task05/Matrix.java
@@ -0,0 +1,39 @@
+package task05;
+
+public class Matrix {
+	private static int n = 7;
+
+	public static void main(String... args) {
+		readArgs(args);
+		printMaitrix(createMatrix());
+	}
+
+	private static void readArgs(String[] args) {
+		for (String arg : args)
+			if (arg.startsWith("n="))
+				n = readInt(arg);
+	}
+
+	private static int readInt(String arg) {
+		return Integer.parseInt(arg.substring(2));
+	}
+
+	private static int[][] createMatrix() {
+		int[][] matrix = new int[n][n];
+		for (int i = 0; i < n; i++)
+			for (int j = 0; j < n; j++)
+				if (i == j || i == n - 1 - j)
+					matrix[i][j] = 1;
+		return matrix;
+	}
+
+	private static void printMaitrix(int[][] matrix) {
+		for (int i = 0; i < n; i++) {
+			System.out.print("(");
+			for (int j = 0; j < n; j++) {
+				System.out.print(matrix[i][j] + " ");
+			}
+			System.out.println(")");
+		}
+	}
+}
