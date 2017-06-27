f06debec0e8d757130d35f6d1f7df877937c9356
Kors <i.korsikov@spb.ant-inform.ru>
2017-06-27 13:00:26 +0300

    2 - refactoring (task 5)

--- 2-oop/src/main/java/university/GroupsManager.java
+++ 2-oop/src/main/java/university/GroupsManager.java
@@ -5,17 +5,22 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class GroupsManager {
+	private Group<Integer> mathsGroup = new Group<>(SubjectMatter.MATHS, 0);
+	private Group<Double> geographyGroup = new Group<>(SubjectMatter.GEOGRAPHY, 0.0);
+	private Group<Byte> physicsGroup = new Group<>(SubjectMatter.PHYSICS, (byte) 0);
 
 	public static void main(String[] args) {
+		new GroupsManager().test();
+	}
 
-		Group<Integer> mathsGroup = new Group<>(SubjectMatter.MATHS, 0);
-		Group<Double> geographyGroup = new Group<>(SubjectMatter.GEOGRAPHY, 0.0);
-		Group<Byte> physicsGroup = new Group<>(SubjectMatter.PHYSICS, (byte) 0);
-
+	private void test() {
 		List<Student> students = IntStream.range(1, 7)
 				.mapToObj(i -> new Student("Student ¹" + i))
 				.collect(Collectors.toList());
+		distributeStudents(students);
+	}
 
+	private void distributeStudents(List<Student> students) {
 		mathsGroup.addStudents(students.get(0), students.get(1), students.get(2));
 		geographyGroup.addStudents(students.get(2), students.get(3), students.get(4));
 		physicsGroup.addStudents(students.get(0), students.get(5));
