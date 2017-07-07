cd4c8300fdb9d2bc4c9170a6f1a3c99caf23aa70
Kors <i.korsikov@spb.ant-inform.ru>
2017-07-07 14:01:58 +0300

    3 - fix separator (task 1)

--- 3-strings/src/main/java/logger/CrazyLogger.java
+++ 3-strings/src/main/java/logger/CrazyLogger.java
@@ -7,18 +7,21 @@ class CrazyLogger {
 	private StringBuilder data = new StringBuilder();
 	private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-YYYY : hh-mm");
 
+	/**
+	 * using ; are not available in this log
+	 */
 	synchronized void write(String s) {
 		LocalDateTime dateTime = LocalDateTime.now();
 		data.append(dateTime.format(formatter))
 				.append(" - ")
-				.append(s)
-				.append("\n");
+				.append(s.replaceAll(";", ","))
+				.append(";");
 	}
 
 	void findAndShow(String text) {
 		String content = data.toString();
 		String lowText = text.toLowerCase();
-		for (String note : content.split("\\n"))
+		for (String note : content.split(";"))
 			if (note.toLowerCase().contains(lowText))
 				System.out.println(note);
 	}
