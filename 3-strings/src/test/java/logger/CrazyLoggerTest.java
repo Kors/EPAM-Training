62ff1793a363c28c02754b499000c0ce3c44411d
Kors <i.korsikov@spb.ant-inform.ru>
2017-07-07 11:40:54 +0300

    3 - negative test (task 1)

--- 3-strings/src/test/java/logger/CrazyLoggerTest.java
+++ 3-strings/src/test/java/logger/CrazyLoggerTest.java
@@ -7,11 +7,15 @@ import org.junit.jupiter.api.Test;
 import java.io.PrintStream;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.StringContains.containsString;
 
 class CrazyLoggerTest {
 
 	private static CrazyLogger log = new CrazyLogger();
+	private PrintStream out = System.out;
+	private ByteOutputStream testStream = new ByteOutputStream();
+	private PrintStream ps = new PrintStream(testStream);
 
 	@BeforeEach
 	void init() {
@@ -23,17 +27,21 @@ class CrazyLoggerTest {
 
 	@Test
 	void findInLog() {
-		PrintStream out = System.out;
-		ByteOutputStream testStream = new ByteOutputStream();
-		PrintStream ps = new PrintStream(testStream);
 		System.setOut(ps);
-
 		log.findAndShow("WARN");
-
 		System.setOut(out);
 		String result = new String(testStream.getBytes()).trim();
 		assertThat(result, containsString("Warn note"));
 		assertThat(result, containsString("Another simple warn note"));
+	}
+
+	@Test
+	void noInfoInLog() {
+		System.setOut(ps);
+		log.findAndShow("dog");
+		System.setOut(out);
+		String result = new String(testStream.getBytes()).trim();
+		assertThat(result, is(""));
 	}
 
 }
