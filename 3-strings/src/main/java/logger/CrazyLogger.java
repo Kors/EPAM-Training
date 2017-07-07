86e20b0fc5accd934b37f1d97f55d5e3ac827fb3
Kors <i.korsikov@spb.ant-inform.ru>
2017-07-07 11:35:06 +0300

    3 - logger (task 1)

--- 3-strings/src/main/java/logger/CrazyLogger.java
+++ 3-strings/src/main/java/logger/CrazyLogger.java
@@ -1,11 +1,25 @@
 package logger;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 class CrazyLogger {
-	StringBuilder data = new StringBuilder();
+	private StringBuilder data = new StringBuilder();
+	private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-YYYY : hh-mm");
 
 	void write(String s) {
+		LocalDateTime dateTime = LocalDateTime.now();
+		data.append(dateTime.format(formatter))
+				.append(" - ")
+				.append(s)
+				.append("\n");
 	}
 
 	void findAndShow(String text) {
+		String content = data.toString();
+		String lowText = text.toLowerCase();
+		for (String note : content.split("\\n"))
+			if (note.toLowerCase().contains(lowText))
+				System.out.println(note);
 	}
 }
