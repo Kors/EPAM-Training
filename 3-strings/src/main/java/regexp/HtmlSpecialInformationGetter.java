49293c95137628dc1506007a655a403767b2feb1
Kors <korsarsmail@mail.ru>
2017-07-18 00:44:23 +0300

    3 - check reg exp

--- 3-strings/src/main/java/regexp/HtmlSpecialInformationGetter.java
+++ 3-strings/src/main/java/regexp/HtmlSpecialInformationGetter.java
@@ -1,33 +1,43 @@
 package regexp;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import lombok.SneakyThrows;
+import lombok.extern.log4j.Log4j2;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
+@Log4j2
 public class HtmlSpecialInformationGetter {
 
+	static final String picRefRegExp = "[Рр]ис((\\.)|(унке)) (\\d+)";
+	static final String sentenceRegExp = "([^(]|^)([А-ЯA-Z].+?[^А-ЯA-Z][.!?])(([<$])|( [^\\d]))";
+
+	String text;
+
+	HtmlSpecialInformationGetter() {
+		text = getText();
+	}
+
 	public static void main(String[] args) throws IOException {
-		File html = new File("3-strings/src/main/resources/Information.html");
-		System.out.println("Path: " + html.getAbsolutePath());
-		Pattern p = Pattern.compile("[Рр]ис((\\.)|(унке)) \\d+");
+		HtmlSpecialInformationGetter informationGetter = new HtmlSpecialInformationGetter();
+		log.info(() -> "Ссылки автора расположены" + (informationGetter.isLinksOrdered() ? "" : " не") + " по порядку");
+	}
+
+	private boolean isLinksOrdered() {
+		return false;
+	}
+
+	@SneakyThrows
+	private static String getText() {
+		File html = new File(HtmlSpecialInformationGetter.class.getResource("/Information.html").getFile());
+		log.info("Html path: " + html.getAbsolutePath());
 		try (BufferedReader f = new BufferedReader(new FileReader(html))) {
-			List<String> l = f.lines().parallel()
-					.filter((s) -> s.matches(".*[Рр]ис((\\.)|(унке)) \\d+.*"))
-					.map(String::trim)
-					.collect(Collectors.toList());
-			System.out.println(l.size());
-			for (String s : l) {
-				Matcher m = p.matcher(s);
-				if (m.find())
-					System.out.println(m.group(0));
-				else
-					throw new RuntimeException("Несоответствие в строке! " + s);
-			}
+			return f.lines().collect(Collectors.joining()); // собираем всё в строку. (считаем что файл достаточно мал для такой возможности.)
 		}
 	}
 }
--- 3-strings/src/test/java/regexp/HtmlSpecialInformationGetterTest.java
+++ 3-strings/src/test/java/regexp/HtmlSpecialInformationGetterTest.java
@@ -1,16 +1,39 @@
 package regexp;
 
+import org.hamcrest.core.Is;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
 import static org.junit.jupiter.api.Assertions.*;
+import static regexp.HtmlSpecialInformationGetter.picRefRegExp;
 
 class HtmlSpecialInformationGetterTest {
 
-	// при поиске "по простому" найдено 82 ссылки на рисунки.
-	// При оптимизациях надо будет проверять что мы никакие не потеряли.
+	private static HtmlSpecialInformationGetter informationGetter;
 
+	@BeforeAll
+	static void setUp() {
+		informationGetter = new HtmlSpecialInformationGetter();
+	}
+
+	// при поиске "по простому" найдено 186 ссылки на рисунки.
+	// При оптимизациях надо проверять что мы никакие не потеряли.
 	@Test
-	void test() {
+	void linkRegExpTest() {
+		assertThat("Wrong link reg.exp.(Count mismatch)", getLinksCount(), is(186));
+	}
+
+	private int getLinksCount() {
+		Matcher ma = Pattern.compile(picRefRegExp).matcher(informationGetter.text);
+		int counter = 0;
+		while (ma.find())
+			counter++;
+		return counter;
 	}
 }
