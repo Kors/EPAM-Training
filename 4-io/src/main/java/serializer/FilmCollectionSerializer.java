828d496059ea15d7d1587071040a48905d9e1e74
Kors <korsarsmail@mail.ru>
2017-07-20 17:54:51 +0300

    5 - tuple (task 4)

--- 4-io/src/main/java/serializer/FilmCollectionSerializer.java
+++ 4-io/src/main/java/serializer/FilmCollectionSerializer.java
@@ -1,4 +1,38 @@
 package serializer;
 
+import lombok.ToString;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
 public class FilmCollectionSerializer {
+
+	public static void main(String[] args) {
+		HashSet<Tuple> filmCollection = load();
+		filmCollection.add(Tuple.get("В погоне за http-сервером", "Корсиков Илья"));
+		System.out.println(filmCollection);
+		save(filmCollection);
+	}
+
+	private static HashSet<Tuple> load() {
+		return new HashSet<>();
+	}
+
+	private static void save(HashSet<Tuple> filmCollection) {
+	}
+
+	@ToString
+	static class Tuple implements java.io.Serializable {
+		private final String film;
+		private final String actor;
+
+		private Tuple(String film, String actor) {
+			this.film = film;
+			this.actor = actor;
+		}
+
+		static Tuple get(String film, String actor) {
+			return new Tuple(film, actor);
+		}
+	}
 }
