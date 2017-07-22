f3475ea8482cea6bf4ab95a227e6a7884c7bb7fc
Kors <i.korsikov@spb.ant-inform.ru>
2017-07-22 15:40:33 +0300

    http-server - using ForkJoinPool

--- http-server/src/main/java/server/HttpServer.java
+++ http-server/src/main/java/server/HttpServer.java
@@ -4,6 +4,7 @@ import lombok.extern.log4j.Log4j2;
 
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.util.concurrent.ForkJoinPool;
 
 @Log4j2
 class HttpServer {
@@ -13,10 +14,10 @@ class HttpServer {
 	public static void main(String... args) {
 		try (ServerSocket serverSocket = new ServerSocket(PORT)) {
 			log.info(() -> "Server started on port: " + serverSocket.getLocalPort() + "\n");
+			ForkJoinPool pool = ForkJoinPool.commonPool();
 			while (!Thread.currentThread().isInterrupted()) {
 				log.debug("Client connected");
-				new Thread(new SocketProcessor(serverSocket.accept())).start();
-				log.debug("done");
+				pool.execute(new SocketProcessor(serverSocket.accept()));
 			}
 		} catch (IOException e) {
 			log.error("Start http-server failed", e);
