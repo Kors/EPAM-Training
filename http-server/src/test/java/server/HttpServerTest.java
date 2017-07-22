1574d4e9a600b2541c8d4832149f3869c6190298
Kors <i.korsikov@spb.ant-inform.ru>
2017-07-22 14:05:09 +0300

    http-server - setUp and tearDown

--- http-server/src/test/java/server/HttpServerTest.java
+++ http-server/src/test/java/server/HttpServerTest.java
@@ -1,33 +1,48 @@
 package server;
 
 import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.Socket;
 
 @Log4j2
 class HttpServerTest {
 
+	static Thread serverThread;
+
 	private int PORT = 8080;
 	private String REQUEST = "GET / HTTP/1.1\r\n\r\n"; // TODO нормальный запрос сделать
 
+	@BeforeAll
+	static void setUp() {
+		serverThread = new Thread(HttpServer::main);
+		serverThread.start();
+	}
+
+	@AfterAll
+	static void tearDown() {
+		serverThread.interrupt();
+	}
+
 	@Test
 	void ping() throws IOException {
-		Thread serverThread = new Thread(HttpServer::main);
-		serverThread.start();
 		try (Socket socket = new Socket("localhost", PORT)) {
 			OutputStream os = socket.getOutputStream();
 			BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 			os.write(REQUEST.getBytes());
 
 			String line;
-			while ((line = br.readLine()) != null && !line.trim().isEmpty()) {
+			while ((line = br.readLine()) != null) {
 				// TODO читаем ответ, надо проверять
 				log.info(line);
 			}
 		}
-		serverThread.interrupt();
 	}
 
 }
