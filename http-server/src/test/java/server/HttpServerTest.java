74f9ba69562f27abef0a1a0239cbc5af849a32ca
Kors <i.korsikov@spb.ant-inform.ru>
2017-07-28 14:42:36 +0300

    http-server - negative response test

--- http-server/src/test/java/server/HttpServerTest.java
+++ http-server/src/test/java/server/HttpServerTest.java
@@ -1,18 +1,16 @@
 package server;
 
+import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
+import org.hamcrest.core.AnyOf;
 import org.hamcrest.core.StringStartsWith;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.util.Properties;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 
@@ -20,9 +18,13 @@ import static org.hamcrest.MatcherAssert.assertThat;
 class HttpServerTest {
 
 	private static Thread serverThread;
+	private Socket clientSocket;
 
-	private static final int PORT = 8080;
-	private static final String[] REQUESTS = {
+	private BufferedWriter clientWriter;
+	private BufferedReader clientReader;
+
+	private static int PORT;
+	private static final String[] OK_REQUESTS = {
 			"GET / HTTP/1.1\n" +
 					"Host: localhost:8090\n" +
 					"Connection: keep-alive\n" +
@@ -40,13 +42,50 @@ class HttpServerTest {
 					"Accept: image/webp,image/apng,image/*,*/*;q=0.8\n" +
 					"Referer: http://localhost:8090/testPage.html\n" +
 					"Accept-Encoding: gzip, deflate, br\n" +
+					"Accept-Language: ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4\n\n",
+			"HEAD /imgs/simpleImg.jpg HTTP/1.1\n" +
+					"Host: localhost:8090\n" +
+					"Connection: keep-alive\n" +
+					"User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36\n" +
+					"Accept: image/webp,image/apng,image/*,*/*;q=0.8\n" +
+					"Referer: http://localhost:8090/testPage.html\n" +
+					"Accept-Encoding: gzip, deflate, br\n" +
 					"Accept-Language: ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4\n\n"
+	};
+	private static final String[] ERR_REQUESTS = {
+			"GET /noSuchFile.txt HTTP/1.1\n" +
+					"Host: localhost:8090\n" +
+					"Connection: keep-alive\n" +
+					"Cache-Control: max-age=0\n" +
+					"Upgrade-Insecure-Requests: 1\n" +
+					"User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36\n" +
+					"Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8\n" +
+					"Accept-Encoding: gzip, deflate, br\n" +
+					"Accept-Language: ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4\n" +
+					"If-Modified-Since: Wed, 21 Oct 2015 07:28:00 GMT\n\n",
+			"POST /imgs/simpleImg.jpg HTTP/1.1\n" +
+					"Host: localhost:8090\n" +
+					"Connection: keep-alive\n" +
+					"User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36\n" +
+					"Accept: image/webp,image/apng,image/*,*/*;q=0.8\n" +
+					"Referer: http://localhost:8090/testPage.html\n" +
+					"Accept-Encoding: gzip, deflate, br\n" +
+					"Accept-Language: ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4\n\n" +
+					"Some data to Post\n"
 	};
 
 	@BeforeAll
 	static void setUp() {
 		serverThread = new Thread(HttpServer::main, "httpServer");
 		serverThread.start();
+		PORT = getPort();
+	}
+
+	@SneakyThrows
+	private static int getPort() {
+		Properties props = new Properties();
+		props.load(HttpServer.class.getResourceAsStream("/http-server.properties"));
+		return Integer.parseInt(props.getProperty("port", "8080"));
 	}
 
 	@AfterAll
@@ -54,25 +93,54 @@ class HttpServerTest {
 		serverThread.interrupt();
 	}
 
-	@DisplayName("Get right response")
-	@ParameterizedTest
-	@MethodSource(names = "getRequests")
-	void okResponseTest(String request) throws IOException {
-		try (Socket socket = new Socket("localhost", PORT)) {
-			OutputStream os = socket.getOutputStream();
-			BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-			os.write(request.getBytes());
-
-			StringBuilder response = new StringBuilder();
-			for (String line; (line = br.readLine()) != null; log.info(line)) {
-				response.append(line);
-			}
-			assertThat(response.toString(), StringStartsWith.startsWith("HTTP/1.1 200 OK"));
-		}
+	@BeforeEach
+	void setUpEach() throws IOException {
+		clientSocket = new Socket("localhost", PORT);
+		clientWriter = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+		clientReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 	}
 
-	private static String[] getRequests() {
-		return REQUESTS;
+	@AfterEach
+	void tearDownEach() throws IOException {
+		clientSocket.close();
+	}
+
+	@DisplayName("Get right response")
+	@ParameterizedTest
+	@MethodSource(names = "getOkRequests")
+	void okResponseTest(String request) throws IOException {
+		clientWriter.write(request);
+		clientWriter.flush();
+		assertThat(getResponse(), StringStartsWith.startsWith("HTTP/1.1 200 OK"));
+	}
+
+	@DisplayName("Get failed response")
+	@ParameterizedTest
+	@MethodSource(names = "getErrRequests")
+	void errResponseTest(String request) throws IOException {
+		clientWriter.write(request);
+		clientWriter.flush();
+		assertThat(getResponse(), AnyOf.anyOf(StringStartsWith.startsWith("HTTP/1.1 404 Not Found"),
+				StringStartsWith.startsWith("HTTP/1.1 501 Not Implemented")));
+	}
+
+	private String getResponse() throws IOException {
+		StringBuilder response = new StringBuilder();
+		for (String line; (line = clientReader.readLine()) != null; ) {
+			response.append(line);
+			log.debug(line);
+		}
+		return response.toString();
+	}
+
+	@SuppressWarnings("unused")
+	private static String[] getOkRequests() {
+		return OK_REQUESTS;
+	}
+
+	@SuppressWarnings("unused")
+	private static String[] getErrRequests() {
+		return ERR_REQUESTS;
 	}
 
 }
