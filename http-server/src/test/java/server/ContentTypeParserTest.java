0a5729ff099ced4aa67505f2fbbaf65d6c3f63c3
Kors <korsarsmail@mail.ru>
2017-07-30 00:19:51 +0300

    http-server - mime test extension

--- http-server/src/test/java/server/ContentTypeParserTest.java
+++ http-server/src/test/java/server/ContentTypeParserTest.java
@@ -11,7 +11,8 @@ class ContentTypeParserTest {
 
 	@DisplayName("Get matched MIME Type")
 	@ParameterizedTest
-	@CsvSource({"index.html, text/html", "page.txt, text/plain", "favicon.ico, image/x-icon", "image.jpg, image/jpeg", "flower.png, image/png"})
+	@CsvSource({"index.html, text/html", "page.txt, text/plain", "favicon.ico, image/x-icon", "image.jpg, image/jpeg",
+			"flower.png, image/png", "some.unknown, application/octet-stream", "noExtension, application/octet-stream",})
 	void getMimeTypeTest(String fileName, String mimeType) {
 		assertThat(ContentTypeParser.getMimeType(fileName), Is.is(mimeType));
 	}
